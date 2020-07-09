763cc3ca9ae22c6269d7dd6e075f9e8812909bde
I29042 <i29042@icsc.com.tw>
2020-07-09 11:15:56 +0800

    feat : Debug先拿掉不必要的Code

--- DummyAkkaNetWebSimpleTest/DummyAkkaNetWeb/Startup.cs
+++ DummyAkkaNetWebSimpleTest/DummyAkkaNetWeb/Startup.cs
@@ -1,4 +1,3 @@
-using Core;
 using DummyAkkaNetWeb.Actor;
 using DummyAkkaNetWeb.Hubs;
 using Microsoft.AspNetCore.Builder;
@@ -26,14 +25,7 @@ namespace DummyAkkaNetWeb
             // SignalIR
             services.AddSignalR();
 
-            services.AddSingleton<ISysAkkaManager>(p =>
-            {
-                // Create the ActorSystem and Dependency Resolver                
-                var actSystem = ActorSystem.Create(AkaSysName, AkkaConfig(AkaSysPort));
-                actSystem.UseServiceProvider(_provider);
-                return new SysAkkaManager(actSystem);
-            });
-
+          
 
             // Register ActorSystem
             services.AddSingleton<ChatHub>();
