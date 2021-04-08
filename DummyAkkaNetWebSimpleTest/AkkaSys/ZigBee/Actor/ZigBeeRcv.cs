08be5a16e4f9d7f0e4308aaa296f8ba22841008f
pal2097 <pal2097@gmail.com>
2021-04-08 14:14:17 +0800

    New ZigBee 外部系統

---
+++ DummyAkkaNetWebSimpleTest/AkkaSys/ZigBee/Actor/ZigBeeRcv.cs
@@ -0,0 +1,17 @@
+﻿using Akka.Actor;
+using AkkaBase;
+using AkkaBase.Base;
+
+namespace AkkaSys.ZigBee.Actor
+{
+    public　class ZigBeeRcv : BaseServerActor
+    {
+        private readonly ISysAkkaManager _akkaManager;
+
+        public ZigBeeRcv(ISysAkkaManager akkaManager, AkkaSysIP akkaSysIp) : base(akkaSysIp)
+        {
+            _akkaManager = akkaManager;
+     
+        }
+    }
+}
---
+++ DummyAkkaNetWebSimpleTest/AkkaSys/ZigBee/ZigBeeMgr.cs
@@ -0,0 +1,14 @@
+﻿using AkkaBase;
+using AkkaBase.Base;
+using AkkaSys.ZigBee.Actor;
+
+namespace AkkaSys.ZigBee
+{
+    public class ZigBeeMgr :　BaseActor
+    {
+        public ZigBeeMgr(ISysAkkaManager akkaManager)
+        {
+            akkaManager.CreateChildActor<ZigBeeRcv>(Context);
+        }
+    }
+}
